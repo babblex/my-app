764e3f41e9c7e297c28e74aa0e47995dc3c2df25
babblex <szucs_alexandra@hotmail.com>
2026-05-14 17:25:46 +0200

    tog typ bort ett space?

--- src/components/MovieListItem/MovieListItem.jsx
+++ src/components/MovieListItem/MovieListItem.jsx
@@ -28,7 +28,7 @@ const MovieListItem = (props) => {
         props.deleteMovie(props.movie.id);
     }
 
-
+    
     return (
         <ListGroup.Item className='list-item'>
             <h3>{props.movie.title}</h3>
