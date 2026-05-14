0a69e8621db8e381949dcfc4ec413f7fc396df93
babblex <szucs_alexandra@hotmail.com>
2026-05-14 17:28:59 +0200

    la till marginal för sorteringsknappar

--- src/components/SortButton/SortButton.jsx
+++ src/components/SortButton/SortButton.jsx
@@ -4,8 +4,8 @@ function SortButton({text, sortOnClick }) {
   //Vi tar emot sortOnClick funktionen som kopplar ihop aktuell sorteringsfunktion i App.jsx beroendes på vilken knapp som klickades på.
   return (
     <>
-    {/* className="me-3" ger marginal mellan knapparna: https://getbootstrap.com/docs/5.1/utilities/spacing/*/}
-      <Button variant="primary" onClick={sortOnClick} className="me-3">{text}</Button>
+    {/* className="me-3 my-2" ger marginal för knapparna: https://getbootstrap.com/docs/5.1/utilities/spacing/*/}
+      <Button variant="primary" onClick={sortOnClick} className="me-3 my-2">{text}</Button>
     </>
   );
 }
